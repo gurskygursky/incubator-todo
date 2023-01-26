8ea6445bdfefe2c377215e957bf4db91398b124d
yegorgursky <yahorhurski@gmail.com>
2023-01-26 19:59:16 +0300

    tasks reducer test, change task title and status tests

--- src/reducers/tasks-reducer.test.ts
+++ src/reducers/tasks-reducer.test.ts
@@ -1,6 +1,6 @@
 import {v1} from 'uuid';
 import {TasksStateType} from './../AppWithReducers';
-import {removeTaskAC, tasksReducer} from './../reducers/tasks-reducer';
+import {changeTaskStatusAC, changeTaskTitleAC, removeTaskAC, tasksReducer} from './../reducers/tasks-reducer';
 
 test('correct task should be removed', () => {
 
@@ -32,4 +32,56 @@ test('correct task should be removed', () => {
             {id: '2', title: 'React Book', isDone: true}
         ]
     });
+});
+
+test('correct task should be change its name', () => {
+
+    const todolistId1 = v1();
+    const todolistId2 = v1();
+
+    const startState: TasksStateType = {
+        [todolistId1]: [
+            {id: '1', title: 'HTML&CSS', isDone: true},
+            {id: '2', title: 'JS', isDone: true}
+        ],
+        [todolistId2]: [
+            {id: '1', title: 'Milk', isDone: true},
+            {id: '2', title: 'React Book', isDone: true}
+        ]
+    };
+
+    const endState = tasksReducer(startState, changeTaskTitleAC(todolistId1, '1', 'xxx'));
+
+    expect(endState[todolistId1][0].title).toBe('xxx');
+    expect(endState[todolistId1][1].title).toBe('JS');
+    expect(endState[todolistId2]).toEqual([
+        {id: '1', title: 'Milk', isDone: true},
+        {id: '2', title: 'React Book', isDone: true}
+    ]);
+});
+
+test('correct task status should be changed', () => {
+
+    const todolistId1 = v1();
+    const todolistId2 = v1();
+
+    const startState: TasksStateType = {
+        [todolistId1]: [
+            {id: '1', title: 'HTML&CSS', isDone: true},
+            {id: '2', title: 'JS', isDone: true}
+        ],
+        [todolistId2]: [
+            {id: '1', title: 'Milk', isDone: true},
+            {id: '2', title: 'React Book', isDone: true}
+        ]
+    };
+
+    const endState = tasksReducer(startState, changeTaskStatusAC(todolistId1, '1', false));
+
+    expect(endState[todolistId1][0].isDone).toBe(false);
+    expect(endState[todolistId1][1].isDone).toBe(true);
+    expect(endState[todolistId2]).toEqual([
+        {id: '1', title: 'Milk', isDone: true},
+        {id: '2', title: 'React Book', isDone: true}
+    ]);
 });
